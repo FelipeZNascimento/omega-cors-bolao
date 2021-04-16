d82d0149e666bf5bd7a37f41853b81bf7ce625da
Felipe <sharpion.k@gmail.com>
2021-04-16 22:30:40 +0100

    Test debug

--- sql/sql.js
+++ sql/sql.js
@@ -2,6 +2,7 @@ var mysql = require('mysql');
 var SQLConfig = require('../app/const/sqlConfig');
 
 var port = process.env.PORT || 8081;
+console.log(SQLConfig.returnConfig(port));
 var connection = mysql.createConnection(SQLConfig.returnConfig(port));
 
 connection.connect(function (err) {
