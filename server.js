c2028f2a3fee22342956139cabdd0890a8bf0bf4
Felipe <sharpion.k@gmail.com>
2021-04-17 12:25:37 +0100

    PLEASE WORK

--- server.js
+++ server.js
@@ -8,43 +8,28 @@ const express = require('express'),
     dotenv = require('dotenv');
 
 dotenv.config();
-
-
-var allowedOrigins = ['https://bolao.omegafox.me', 'http://localhost'];
-// var corsOptions = {
-//     origin: function (origin, callback) {
-//         if (allowedOrigins.indexOf(origin) !== -1) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('Not allowed by CORS'))
-//         }
-//     },
-//     methods: ['POST', 'GET', 'OPTIONS', 'HEAD'],
-//     credentials: true
-// };
-
-var corsOptions = {
-    origin: allowedOrigins,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-  
-app.use(cors(corsOptions));
 app.options('*', cors());
+app.use(cors({
+    origin: ['http://localhost', 'https://bolao.omegafox.me/', 'http://bolao.omegafox.me/'],
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    credentials: true
+}));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 var sessionStore = new MySQLStore(SQLConfig.returnConfig(port));
 const sessionSecret = process.env.SESSION_SECRET;
 
-const twentyEightDays = 28 * 24 * 60 * 60 * 1000;
+const sevenDays = 7 * 24 * 60 * 60 * 1000;
 
 app.use(session({
     key: 'omega-cors-bolao-nfl-session',
     secret: sessionSecret,
     fetchs: 0,
     cookie: {
-        maxAge: twentyEightDays,
-        secure: process.env.SECURE_COOKIES
+        maxAge: sevenDays,
+        // secure: true
     },
     store: sessionStore,
     resave: false,
@@ -56,7 +41,5 @@ app.listen(port, function () {
     console.log('CORS-enabled web server listening on port ' + port);
 });
 
-console.log('API server started on: ' + port);
-
-var routes = require('./app/routes/appRoutes'); //importing route
+var routes = require('./app/routes/appRoutes'); //importing route 
 routes(app); //register the route
