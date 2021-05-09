18a45f2177ced6ad0018a53c0a80796152782063
Felipe <sharpion.k@gmail.com>
2021-05-09 22:06:01 +0100

    Removing unused routes + refactoring old ones

--- app/routes/appRoutes.js
+++ app/routes/appRoutes.js
@@ -1,14 +1,13 @@
 'use strict';
 module.exports = function (app) {
     const betsController = require('../controller/bets');
-    const cookiesController = require('../controller/cookies');
     const configController = require('../controller/config');
     const matchController = require('../controller/match');
     const rankingController = require('../controller/ranking');
     const teamController = require('../controller/team');
     const userController = require('../controller/user');
 
-    // Matches Routing
+    // Initial Config Routing
     app.route('/bolaonfl/defaultConfig/')
         .get(configController.default)
 
@@ -29,28 +28,24 @@ module.exports = function (app) {
     app.route('/bolaonfl/users/')
         .get(userController.listAll)
 
-    app.route('/bolaonfl/usercheck/')
-        .get(userController.check)
+    app.route('/bolaonfl/user/update/')
+        .post(userController.update)
+
+    app.route('/bolaonfl/user/register/')
+        .post(userController.register)
+
+    app.route('/bolaonfl/user/login/')
+        .post(userController.login)
+
+    app.route('/bolaonfl/user/logout/')
+        .get(userController.logout)
 
     app.route('/bolaonfl/user/:id/')
         .get(userController.listById)
 
-    app.route('/bolaonfl/register/')
-        .post(userController.register)
-
-    app.route('/bolaonfl/login/')
-        .post(userController.login)
-
-    app.route('/bolaonfl/logout/')
-        .get(userController.logout)
-
     // Teams routing
     app.route('/bolaonfl/teams/')
         .get(teamController.listAll)
-
-    // Cookies Routing
-    app.route('/bolaonfl/cookies/')
-        .post(cookiesController.update)
 
     // Ranking Routing
     app.route('/bolaonfl/ranking/season/:season')
